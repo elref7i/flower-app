41b279380fb368ed31047bf3cb5e7ccd55336809
Ahmed Refai <momo3956248@gmail.com>
2025-05-26 10:23:12 +0300

    feat/button: add full styles for dark & light modes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,45 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils/tailwind-merge";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2  whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2  whitespace-nowrap rounded-md text-sm font-medium ring-offset-background  transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:disabled:text-zinc-600",
   {
     variants: {
       variant: {
-        default:
-          "bg-maroon-600 text-primary-foreground hover:bg-maroon-700 disabled:bg-zinc-300 disabled:text-zinc-500",
-        secondary:
-          "bg-maroon-50 text-maroon-600 hover:bg-maroon-100 disabled:bg-zinc-300 disabled:text-zinc-500",
-        outline:
-          "border-[1px] border-maroon-600 bg-maroon-50 text-marron-600 hover:bg-maroon-100 disabled:border-zinc-300 disabled:bg-zinc-100 disabled:text-zinc-400",
-        subtle:
-          "border-[1px] border-zinc-400 bg-zinc-50 hover:bg-zinc-100 disabled:bg-zinc-100 disabled:text-zinc-400",
-        destructive:
-          "bg-red-600 text-destructive-foreground hover:bg-red-700 disabled:bg-zinc-300 disabled:text-zinc-500",
-        ghost:
-          "hover:bg-zinc-100 hover:text-accent-foreground disabled:bg-zinc-100 disabled:text-zinc-400",
+        default: cn(
+          "bg-maroon-600 text-primary-foreground hover:bg-maroon-700",
+          "disabled:bg-zinc-300 disabled:text-zinc-500",
+          "dark:bg-softpink-300 dark:hover:bg-softpink-400 dark:disabled:bg-zinc-700",
+        ),
+
+        secondary: cn(
+          "bg-maroon-50 text-maroon-600 hover:bg-maroon-100",
+          "disabled:bg-zinc-300 disabled:text-zinc-500",
+          "dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:text-softpink-300 dark:disabled:bg-zinc-700",
+        ),
+        outline: cn(
+          "border-[1px] border-maroon-600 bg-maroon-50 text-maroon-600 hover:bg-maroon-100",
+          "disabled:border-zinc-300 disabled:bg-zinc-100 disabled:text-zinc-400",
+          "dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:text-softpink-300 dark:border-zinc-600 dark:disabled:bg-zinc-800",
+        ),
+        subtle: cn(
+          "border-[1px] border-zinc-400 bg-zinc-50 hover:bg-zinc-100",
+          "disabled:bg-zinc-100 disabled:text-zinc-400",
+          "dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:border-zinc-500 dark:disabled:bg-zinc-800",
+        ),
+        destructive: cn(
+          "bg-red-600 text-destructive-foreground hover:bg-red-700",
+          " disabled:bg-zinc-300 disabled:text-zinc-500",
+          "dark:bg-red-500 dark:hover:bg-red-600 dark:disabled:bg-zinc-700",
+        ),
+        ghost: cn(
+          "hover:bg-zinc-100 hover:text-accent-foreground",
+          " disabled:bg-zinc-100 disabled:text-zinc-400",
+          " dark:hover:bg-zinc-700 dark:disabled:bg-zinc-700",
+        ),
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "line- font-bold h-10 leading-[150%] w-[181px] px-4 py-[10px] gap-[10px]",
+        default: "text-sm font-bold h-10 leading-[150%] w-[181px] px-4 py-[10px] gap-[10px]",
         sm: "h-9 w-[160px] rounded-md px-3",
         lg: "h-10 rounded-md px-8",
         icon: "h-10 w-10",
