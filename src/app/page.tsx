025ad2cee68228f6bb5dc122d0f6b8c27e94fb64
Ahmed Refai <momo3956248@gmail.com>
2025-05-28 13:44:49 +0300

    clear page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,10 @@
-"use client";
-import { ModeToggle } from "@/components/common/theme-toggle";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import { toast } from "sonner";
-
 export default function Home() {
   return (
     <div className="p-20">
       <main className="space-y-4">
-        <Button variant={"outline"} className="">
-          Refai
-        </Button>
-        <Input type="email" placeholder="Email" />
-        <Textarea placeholder="input text" />
-        <Select>
-          <SelectTrigger className="">
-            <SelectValue placeholder="Select a fruit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <ModeToggle />
-        <div className="space-x-2">
-          <Label error={true}>Input</Label>
-          <Badge variant={"subtle"}>Badge</Badge>
-        </div>
-
-        <Button
-          variant="outline"
-          onClick={() =>
-            toast("Event has been created", {
-              action: {
-                label: "",
-                onClick: () => console.log("Undo"),
-              },
-            })
-          }
-        >
-          Show Toast
-        </Button>
-
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious href="#" />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">1</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#" isActive>
-                2
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">3</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext href="#" />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+        <h1 className="text-7xl font-bold text-center text-maroon-500 dark:text-softpink-500">
+          Flower App
+        </h1>
       </main>
     </div>
   );
