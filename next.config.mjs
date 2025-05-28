3e024f2cf48514303123710bd16f675727a8369c
Ahmed Refai <momo3956248@gmail.com>
2025-05-28 18:59:30 +0300

    feat/optimizing-image: config local & remote patterns in your next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    localPatterns: [
+      {
+        pathname: "/public/assets/imgs/**",
+      },
+    ],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flower.elevateegy.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
