fa95ce76f7d214643b64afafe10caff3caa22bb1
ehab <ftwehab@gmail.com>
2025-05-31 20:30:18 +0300

    feat/auth: update home page & sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { cn } from "@/lib/utils/tailwind-merge";
 import { useTheme } from "next-themes";
-import { Toaster as Sonner } from "sonner"; // من ShadCN برضو
+import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
