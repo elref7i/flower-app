f30598d7c13dafefabb031a925edea9749d6103a
Mahmoud Reda <sim.mahmoud.reda@gmail.com>
2025-06-19 14:16:19 +0300

    apply refinements

--- src/components/layout/header/components/navigation-header.tsx
+++ src/components/layout/header/components/navigation-header.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
 import { links } from "../constants/links";
-import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils/cn";
+import { usePathname } from "@/i18n/navigation";
 
 export default function NavigationHeader() {
   // Navigation
