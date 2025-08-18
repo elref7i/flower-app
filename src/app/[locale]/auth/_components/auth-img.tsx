28b5a5ff4648f1c98e1c2bb78f4fc227229ac634
Ahmed Refai <momo3956248@gmail.com>
2025-08-18 21:51:29 +0300

    refactor(auth): enhance AuthPageImg component with dynamic height and improve layout structure

--- src/app/[locale]/auth/_components/auth-img.tsx
+++ src/app/[locale]/auth/_components/auth-img.tsx
@@ -1,6 +1,21 @@
+"use client";
 import Image from "next/image";
 import img from "@assets/imgs/image 1.png";
+import { usePathname } from "@/i18n/navigation";
+import { cn } from "@/lib/utils/cn";
 
 export default function AuthPageImg() {
-  return <Image src={img} className="w-full  object-fill" alt="Auth Image" />;
+  const pathName = usePathname();
+  console.log(pathName);
+
+  return (
+    <section
+      className={cn(
+        pathName === "/auth/register" ? "h-full" : "h-screen",
+        "hidden md:block md:col-span-6 bg-red-700",
+      )}
+    >
+      <Image src={img} className={cn("w-full h-full object-fill")} alt="Auth Image" />
+    </section>
+  );
 }
--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -3,11 +3,11 @@ import AuthPageImg from "./_components/auth-img";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <main className=" grid grid-cols-12">
-      <section className="col-span-6  flex justify-center items-center">{children}</section>
-      <section className="hidden md:block md:col-span-6">
-        <AuthPageImg />
-      </section>
+    <main className="grid grid-cols-12">
+      <section className="col-span-6">{children}</section>
+
+      {/* Image Auth */}
+      <AuthPageImg />
     </main>
   );
 }
